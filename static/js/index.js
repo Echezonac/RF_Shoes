a2ad5ceab0b41e0a58a0a3bcee34d9bb155af90c
Bedeman <hampoherobede@gmail.com>
2021-09-27 14:45:18 +0100

    initial commit

---
+++ static/js/index.js
@@ -0,0 +1,368 @@
+
+
+
+
+var change = document.getElementById('changeImg')
+  function nude(){
+    change.src='static/images/nude.jpg'
+  }
+  var change = document.getElementById('changeImg')
+  function red(){
+    change.src='static/images/nd-wtf-10-Aug1615_440x.jpg'
+  }
+  var change = document.getElementById('changeImg')
+  function blue(){
+    change.src='static/images/blue.jpg'
+  }
+
+
+  
+  var img = document.getElementById('changeimg')
+  function first(){
+    img.src='static/images/1blue.jpg'
+  }
+  var img = document.getElementById('changeimg')
+  function second(){
+    img.src='static/images/1black.jpg'
+  }
+  var img = document.getElementById('changeimg')
+  function third(){
+    img.src='static/images/1cream.jpg'
+  }
+  
+
+
+  var dee = document.getElementById('img3')
+  function pink(){
+    dee.src='static/images/3pink.jpg'
+  }
+  var dee = document.getElementById('img3')
+  function black(){
+    dee.src='static/images/3black.jpg'
+  }
+  var dee= document.getElementById('img3')
+  function grey(){
+    dee.src='static/images/3grey.jpg'
+  }
+  var dee = document.getElementById('img3')
+  function brown(){
+    dee.src='static/images/3brown.jpg'
+  }
+
+
+  var vee = document.getElementById('img4')
+  function blacks(){
+    vee.src='static/images/4black.jpg'
+  }
+  var vee = document.getElementById('img4')
+  function nudee(){
+    vee.src='static/images/4nude.jpg'
+  }
+  var vee= document.getElementById('img4')
+  function whites(){
+    vee.src='static/images/4white.jpg'
+  }
+
+
+  var ree = document.getElementById('fur')
+  function bluee(){
+    ree.src='static/images/01blue.jpg'
+  }
+  var ree = document.getElementById('fur')
+  function blk(){
+    ree.src='static/images/01black.jpg'
+  }
+  var ree= document.getElementById('fur')
+  function whit(){
+    ree.src='static/images/01cream.jpg'
+  }
+
+
+  var gee = document.getElementById('fure')
+  function blu(){
+    gee.src='static/images/crossblue.jpg'
+  }
+  var gee = document.getElementById('fure')
+  function whe(){
+    gee.src='static/images/crosscream.jpg'
+  }
+  var gee= document.getElementById('fure')
+  function blac(){
+    gee.src='static/images/crossblk.jpg'
+  }
+  var gee= document.getElementById('fure')
+  function purp(){
+    gee.src='static/images/crossrain.jpg'
+  }
+  
+
+
+  var jee = document.getElementById('fre')
+  function blcs(){
+    jee.src='static/images/03black.jpg'
+  }
+  var jee = document.getElementById('fre')
+  function browe(){
+    jee.src='static/images/03brown.jpg'
+  }
+  var jee= document.getElementById('fre')
+  function wht(){
+    jee.src='static/images/03white.jpg'
+  }
+
+  var bee = document.getElementById('der')
+  function bac(){
+    bee.src='static/images/04black.jpg'
+  }
+  var bee = document.getElementById('der')
+  function bre(){
+    bee.src='static/images/04lilac.jpg'
+  }
+  var bee= document.getElementById('der')
+  function pup(){
+    bee.src='static/images/04rain.jpg'
+  }
+
+  var hee = document.getElementById('ter')
+  function bluer(){
+    hee.src='static/images/05blue.jpg'
+  }
+  var hee = document.getElementById('ter')
+  function noir(){
+    hee.src='static/images/05black.jpg'
+  }
+  var hee= document.getElementById('ter')
+  function wid(){
+    hee.src='static/images/05cream.jpg'
+  }
+
+
+  var der = document.getElementById('ver')
+ 
+  var der= document.getElementById('ver')
+  function bac(){
+    der.src='static/images/06black.jpg'
+  }
+ 
+  var der= document.getElementById('ver')
+  function rainbow(){
+    der.src='static/images/06rain.jpg'
+  }
+
+
+  var zee = document.getElementById('brea')
+  function leo(){
+    zee.src='static/images/07brown.jpg'
+  }
+  var zee = document.getElementById('brea')
+  function gri(){
+    zee.src='static/images/07grey.jpg'
+  }
+  var zee= document.getElementById('brea')
+  function rainbo(){
+    zee.src='static/images/07rain.jpg'
+  }
+
+
+  var sew = document.getElementById('per')
+  function blacee(){
+    sew.src='static/images/08black.jpg'
+  }
+  var sew = document.getElementById('per')
+  function light(){
+    sew.src='static/images/08blue.jpg'
+  }
+  var sew= document.getElementById('per')
+  function pin(){
+    sew.src='static/images/08pink.jpg'
+  }
+  var sew= document.getElementById('per')
+  function gray(){
+    sew.src='static/images/08grey.jpg'
+  }
+
+
+  var sep = document.getElementById('fret')
+  function bl(){
+    sep.src='static/images/09black.jpg'
+  }
+  var sep = document.getElementById('fret')
+  function gold(){
+    sep.src='static/images/09gold.jpg'
+  }
+  var sep= document.getElementById('fret')
+  function silver(){
+    sep.src='static/images/09silver.jpg'
+  }
+  var sep= document.getElementById('fret')
+  function lbrown(){
+    sep.src='static/images/09brown.jpg'
+  }
+
+  var wee = document.getElementById('heel01')
+  function heel(){
+    wee.src='static/images/heel01brown.jpg'
+  }
+  var wee = document.getElementById('heel01')
+  function heel1(){
+    wee.src='static/images/heel01black.jpg'
+  }
+
+  var no = document.getElementById('heel02')
+  function heel3(){
+    no.src='static/images/heel02rose.jpg'
+  }
+  var no = document.getElementById('heel02')
+  function heel4(){
+    no.src='static/images/heei02white.jpg'
+  }
+
+  var n = document.getElementById('heel03')
+  function heel5(){
+    n.src='static/images/heel03black.jpg'
+  }
+  var n = document.getElementById('heel03')
+  function heel6(){
+    n.src='static/images/heel03brown.jpg'
+  }
+
+  var r = document.getElementById('heel04')
+  function heel7(){
+    r.src='static/images/heel04black.jpg'
+  }
+  var r = document.getElementById('heel04')
+  function heel8(){
+    r.src='static/images/heel04brown.jpg'
+  }
+
+  var t = document.getElementById('heel05')
+  function heel9(){
+    t.src='static/images/heel05black.jpg'
+  }
+  var t = document.getElementById('heel05')
+  function heel10(){
+    t.src='static/images/heel05brown.jpg'
+  }
+
+  var y = document.getElementById('heel06')
+  function heel11(){
+    y.src='static/images/heel06black.jpg'
+  }
+  var y = document.getElementById('heel06')
+  function heel12(){
+    y.src='static/images/heel06cream.jpg'
+  }
+  
+  var i = document.getElementById('heel07')
+  function heel13(){
+    i.src='static/images/heel07black.jpg'
+  }
+  var i = document.getElementById('heel07')
+  function heel14(){
+    i.src='static/images/heel07cream.jpg'
+  }
+
+  var g = document.getElementById('heel08')
+  function heel15(){
+    g.src='static/images/heel08black.jpg'
+  }
+  var g = document.getElementById('heel08')
+  function heel16(){
+    g.src='static/images/heel08cream.jpg'
+  }
+  var g = document.getElementById('heel08')
+  function heel17(){
+    g.src='static/images/heel08white.jpg'
+  }
+
+  var d = document.getElementById('ert')
+  function sblk(){
+    d.src='static/images/s03black.jpg'
+  }
+  var d = document.getElementById('ert')
+  function spup(){
+    d.src='static/images/s03purple.jpg'
+  }
+ 
+
+  var f = document.getElementById('train1')
+  function s1(){
+    f.src='static/images/s04white.jpg'
+  }
+  var f = document.getElementById('train1')
+  function s2(){
+    f.src='static/images/s04brown.jpg'
+  }
+ 
+  var c = document.getElementById('train2')
+  function s3(){
+    c.src='static/images/s06white.png'
+  }
+  var c = document.getElementById('train2')
+  function s4(){
+    c.src='static/images/s06black.jpg'
+  }
+  var c = document.getElementById('train2')
+  function s5(){
+    c.src='static/images/s06brown.jpg'
+  }
+
+  var v = document.getElementById('train3')
+  function s6(){
+    v.src='static/images/s07white.jpg'
+  }
+  var v = document.getElementById('train3')
+  function s7(){
+    v.src='static/images/s07black.jpg'
+  }
+  var v = document.getElementById('train3')
+  function s8(){
+    v.src='static/images/s07brown.jpg'
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+  var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+    return new bootstrap.Offcanvas(offcanvasEl)
+  })
+
+      function addCart(){
+        var cart = document.querySelector(".offcanvas")
+        var button = document.querySelectorAll("* .add_to_cart_btn")
+        $(button).click(function(e){
+          let parent = e.target.parentNode
+          console.log(parent)
+    
+    
+        })
+    
+      }
+    
+    
+      addCart()
+
+
+
+
+  
